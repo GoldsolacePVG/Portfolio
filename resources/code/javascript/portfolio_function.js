313d11acf765a71cb3f445ddf568a6e8b6239fc9
GoldsolacePVG <rodrigomicopliego@gmail.com>
2023-11-07 01:32:12 +0100

    Functionality

--- resources/code/javascript/portfolio_function.js
+++ resources/code/javascript/portfolio_function.js
@@ -1,7 +1,26 @@
+var img_index = 0;
+var my_img_list = new Array();
+// Src
+my_img_list[0] = "../../img/projects/MH_Web.png";
+
+ChangeImg(img_index);
+
+function AddIndex(){
+  img_index += 1;
+  if(img_index > my_img_list.length - 1){img_index = 0;}
+  ChangeImg(img_index);
+}
+
+function SubstractIndex(){
+  img_index -= 1;
+  if(img_index < 0){img_index = my_img_list.length - 1;}
+  ChangeImg(img_index);
+}
+
 function ChangeImg(index){
   var aux_doc = document.getElementById("project_img");
+  var aux_doc_img = document.getElementById("project_img").src;
 
-  var my_img_list = new Array();
-
-  my_img_list[0] = ""; 
+  aux_doc_img = my_img_list[0];
+  document.getElementById("project_img").src = aux_doc_img;
 }
